41951dff64a382bd68d2ac1f10648ae3d9103f4f
nachovg <nachovg.12@gmail.com>
2018-10-03 22:08:00 -0600

    First commit

---
+++ Assets/BordeInferior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BordeInferior : MonoBehaviour {
+
+	// Update is called once per frame
+	void OnTriggerEnter2D (Collider2D col) {
+		if(col.CompareTag("Bolita")){
+			col.SendMessage ("Iniciar");
+		}
+		
+	}
+}
